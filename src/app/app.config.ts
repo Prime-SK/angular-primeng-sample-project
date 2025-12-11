b6cd900d6d4bbf0fbdf79067d36578fd585022b2
Thathsara <140164919+Prime-SK@users.noreply.github.com>
2025-12-11 16:00:23 +0530

    feat: refactor client form and table components with improved validation and styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+import Material from '@primeng/themes/material';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: Aura
+                preset: Aura,
+                options: {
+                    darkModeSelector: false || 'none',
+                }
             }
         })
     ]
